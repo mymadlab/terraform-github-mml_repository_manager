1c6ac79522c93c97bfd88724114151233527ae3a
biggiebk <biggiebk@mymadlab.net>
2024-04-27 14:05:53 -0700

    Version referencing

--- providers.tf
+++ providers.tf
@@ -6,9 +6,9 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = ">=6.0.1"
+      version = ">= 6.0.1"
     }
   }
 
-  required_version = ">= v1.7.5"
+  required_version = ">= 1.7.5"
 }
